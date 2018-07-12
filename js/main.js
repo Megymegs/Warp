965c04bcfc42cf7946af015391446843a168974a
megymegs <megymegs@gmail.com>
2018-07-12 15:55:47 +0200

    adding content, adding icons amd styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-// Spreadsheet 
+// Spreadsheet
 
 $(document).ready(function() {
-    $('#spreadsheet-data').DataTable( {
-        select: true
-    });
+  $("#spreadsheet-data").DataTable({
+    select: true,
+    language: {
+        info: "_START_ to _END_ of _TOTAL_",
+        search: "",
+        aria: {
+            "sortAscending":  ": activate to sort column ascending",
+            "sortDescending": ": activate to sort column descending"
+        },
+        paging: true,
+        pagingType: "full_numbers",
+        paginate: {
+            first: '<i class="fa fa-fw fa-chevron-double-left">',
+            next: '<i class="fa fa-fw fa-chevron-right">',
+            previous: '<i class="fa fa-fw fa-chevron-left">',
+            last: '<i class="fa fa-fw fa-chevron-double-right">'
+        }
+    }
+  });
 });
