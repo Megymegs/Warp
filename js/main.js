9c65def05f74015fd540f01c94c044aa4ffde228
megymegs <megymegs@gmail.com>
2018-07-15 12:14:30 +0200

    editing modal and adding tooltip to rows

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ FontAwesomeConfig = {
 $(document).ready(function() {
   $("#spreadsheet-data").DataTable({
     select: true,
+    responsive: true,
     language: {
         info: "_START_ to _END_ of _TOTAL_",
         search: "",
@@ -23,20 +24,39 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
-    $('#spreadsheet-data').DataTable( {
-        responsive: {
-            details: {
-                display: $.fn.dataTable.Responsive.display.modal( {
-                    header: function ( row ) {
-                        var data = row.data();
-                        return 'Details for '+data[0]+' '+data[1];
-                    }
-                } ),
-                renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
-                    tableClass: 'table'
-                } )
-            }
-        }
-    } );
-} );
+
+(function () {
+    $(document).on('shown.bs.modal', function () {
+        createCard();
+    });
+    // Trigger Modal Containing the Table
+    $(document).on('click', '.add', function () {
+        // Show Modal
+        $('.gift-card').modal('show');
+    });
+    $(document).on('click', '.next-modal', function () {
+        // Show Modal
+        $('.step-2').css('display', 'block');
+        $('.step-1').css('display', 'none' );
+    });
+    $(document).on('click', '.back-modal', function () {
+        // Show Modal
+        $('.step-2').css('display', 'none');
+        $('.step-1').css('display', 'flex' );
+    });
+    $(document).on('click', '.next-modal-1', function () {
+        // Show Modal
+        $('.step-3').css('display', 'block');
+        $('.step-2').css('display', 'none' );
+        $('.step-1').css('display', 'none' );
+    });
+    $(document).on('click', '.back-modal-1', function () {
+        // Show Modal
+        $('.step-3').css('display', 'none');
+        $('.step-2').css('display', 'block' );
+        $('.step-1').css('display', 'none' );
+    });
+})()
+function createCard() {
+    $('.gift-card').css('display', 'block');
+}
